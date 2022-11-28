d2ee5db316d7a1558c2fc19ef4f06d1238b5be42
Perry Kwarteng <perrykwarteng8@gmail.com>
2022-11-28 04:23:24 +0000

    styled the nav and also adde the functionality

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let open = document.querySelector(".menu");
+let close = document.querySelector(".close-menu");
+let navigation = document.querySelector(".navigation");
+
+
+function openMenu(){
+    if(open.style.display = "none"){
+        navigation.style.display = "block";
+    }else{
+        navigation.style.display = "none";
+    }
+}
+
+function closeMenu(){
+    if(close.style.display = "block"){
+        navigation.style.display = "none";
+        open.style.display = "block"
+    }else{
+        navigation.style.display = "block";
+    }
+}
+
+open.addEventListener("click", openMenu);
+close.addEventListener("click", closeMenu);
